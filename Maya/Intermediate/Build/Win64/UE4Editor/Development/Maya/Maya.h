50f73e7a23965902d5bd2de40daa3d518e1c00ea
Jesus Christ <stockaccount109@yahoo.com>
2016-10-31 11:04:55 -0400

    Initial Commit (No changes, original source)

---
+++ Maya/Intermediate/Build/Win64/UE4Editor/Development/Maya/Maya.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_MAYA_H__
+#define __AUTO_MAYA_H__
+//Last Write: 10/31/2016 2:59:32 PM +00:00
+#include "I:\T H E S I S\Test_Scenes\MayaThesis\MayaThesis\Maya\Source\Maya\Maya.h"
+#endif//__AUTO_MAYA_H__
---
+++ Maya/Intermediate/Build/Win64/UE4Editor/Development/Maya/PCH.Maya.h.cpp
@@ -0,0 +1,1 @@
+#include "I:\T H E S I S\Test_Scenes\MayaThesis\MayaThesis\Maya\Source\Maya\Maya.h"
